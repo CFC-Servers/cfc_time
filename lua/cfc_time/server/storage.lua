e57aa81e3ad3b16164b84dae4a8bad63b02c9211
PLally <11771225+PLally@users.noreply.github.com>
2020-08-27 03:13:19 -0400

    remove merge conflict

--- lua/cfc_time/server/storage.lua
+++ lua/cfc_time/server/storage.lua
@@ -2,11 +2,6 @@ CFCTime.Storage = {}
 
 local storageType = CFCTime.Config.get( "storageType" )
 
-<<<<<<< HEAD:lua/cfc_time/server/storage.lua
-CFCTime.Storage.realm = "cfctest"
-include( "storage_options/sqlite.lua" )
-=======
 CFCTime.Storage.realm =  CFCTime.Config.get( "realm" )
->>>>>>> master:lua/cfc_time/server/time/sv_storage.lua
 
 include( "storage_options/"..storageType..".lua" )
